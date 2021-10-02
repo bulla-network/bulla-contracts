4161c8a30e111b4fdfd3fa26f5172395c450b4c2
Colin Nielsen <colinrnielsen@gmail.com>
2021-10-02 13:07:47 -0600

    change weth contract/ deploy script

--- contracts/utils/weth.sol
+++ contracts/utils/weth.sol
@@ -1,68 +1,118 @@
 //SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.3;
 
-contract WETH9 {
-    string public name = "Wrapped Ether";
-    string public symbol = "WETH";
-    uint8 public decimals = 18;
+interface Token {
+    function balanceOf(address _owner) external view returns (uint256 balance);
 
-    event Approval(address indexed src, address indexed guy, uint256 wad);
-    event Transfer(address indexed src, address indexed dst, uint256 wad);
-    event Deposit(address indexed dst, uint256 wad);
-    event Withdrawal(address indexed src, uint256 wad);
+    function transfer(address _to, uint256 _value)
+        external
+        returns (bool success);
 
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool success);
 
-    fallback() external {
-        deposit();
+    function approve(address _spender, uint256 _value)
+        external
+        returns (bool success);
+
+    function allowance(address _owner, address _spender)
+        external
+        view
+        returns (uint256 remaining);
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+}
+
+contract WETH is Token {
+    uint256 private constant MAX_UINT256 = 2**256 - 1;
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowed;
+    uint256 public totalSupply;
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+
+    constructor(
+        uint256 _initialAmount,
+        string memory _tokenName,
+        uint8 _decimalUnits,
+        string memory _tokenSymbol
+    ) {
+        balances[msg.sender] = _initialAmount;
+        totalSupply = _initialAmount;
+        name = _tokenName;
+        decimals = _decimalUnits;
+        symbol = _tokenSymbol;
     }
 
-    function deposit() public payable {
-        balanceOf[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
-    }
-
-    function withdraw(uint256 wad) public {
-        require(balanceOf[msg.sender] >= wad);
-        balanceOf[msg.sender] -= wad;
-        payable(msg.sender).transfer(wad);
-        emit Withdrawal(msg.sender, wad);
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return address(this).balance;
-    }
-
-    function approve(address guy, uint256 wad) public returns (bool) {
-        allowance[msg.sender][guy] = wad;
-        emit Approval(msg.sender, guy, wad);
+    function transfer(address _to, uint256 _value)
+        public
+        override
+        returns (bool success)
+    {
+        require(
+            balances[msg.sender] >= _value,
+            "token balance is lower than the value requested"
+        );
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value); //solhint-disable-line indent, no-unused-vars
         return true;
-    }
-
-    function transfer(address dst, uint256 wad) public returns (bool) {
-        return transferFrom(msg.sender, dst, wad);
     }
 
     function transferFrom(
-        address src,
-        address dst,
-        uint256 wad
-    ) public returns (bool) {
-        require(balanceOf[src] >= wad);
-
-        if (
-            src != msg.sender && allowance[src][msg.sender] != type(uint128).max
-        ) {
-            require(allowance[src][msg.sender] >= wad);
-            allowance[src][msg.sender] -= wad;
+        address _from,
+        address _to,
+        uint256 _value
+    ) public override returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(
+            balances[_from] >= _value && allowance >= _value,
+            "token balance or allowance is lower than amount requested"
+        );
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
         }
-
-        balanceOf[src] -= wad;
-        balanceOf[dst] += wad;
-
-        emit Transfer(src, dst, wad);
-
+        emit Transfer(_from, _to, _value); //solhint-disable-line indent, no-unused-vars
         return true;
+    }
+
+    function balanceOf(address _owner)
+        public
+        view
+        override
+        returns (uint256 balance)
+    {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        override
+        returns (bool success)
+    {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function allowance(address _owner, address _spender)
+        public
+        view
+        override
+        returns (uint256 remaining)
+    {
+        return allowed[_owner][_spender];
     }
 }
--- scripts/deploy-hardhat.ts
+++ scripts/deploy-hardhat.ts
@@ -1,23 +1,55 @@
+import { BigNumber, Contract, utils } from "ethers";
 import hre from "hardhat";
-import { deployCreator } from "./deploy-v1";
+import { WETH } from "../typechain/WETH";
+import WETHArtifact from "../artifacts/contracts/utils/WETH.sol/WETH.json";
 
-const tokenDeployer = async () => {
-  const { getNamedAccounts, ethers } = hre;
-  const [account1] = await ethers.getSigners();
-  const [signer] = await ethers.getSigners();
-  const WETH9 = await ethers.getContractFactory("WETH9");
-  const weth = await WETH9.deploy();
-  await weth.deployed();
-  await weth.deposit({ value: ethers.utils.parseEther("1"), from: account1 });
-  await weth.withdrawl({ value: ethers.utils.parseEther("1"), from: account1 });
-  console.log("sent 1 weth to account 1");
+const hardhatDeploy = async () => {
+  const { deployments, getNamedAccounts, ethers, getUnnamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const [, account1] = await ethers.getSigners();
+
+  const { address: managerAddress } = await deploy("BullaManager", {
+    from: deployer,
+    args: [
+      ethers.utils.formatBytes32String("BullaManager v1"),
+      "0x89e03E7980C92fd81Ed3A9b72F5c73fDf57E5e6D",
+      0,
+    ],
+    log: true,
+  });
+
+  const { address: implementAddress } = await deploy("BullaClaimERC20", {
+    from: deployer,
+    log: true,
+  });
+
+  const { address: bankerAddress } = await deploy("BullaBanker", {
+    from: deployer,
+    log: true,
+    args: [managerAddress, implementAddress],
+  });
+
+  const { address: wethAddress } = await deploy("WETH", {
+    from: account1.address,
+    args: [utils.parseEther("10"), "Wrapped Ether", BigNumber.from(18), "WETH"],
+  });
+  const WETH = new Contract(wethAddress, WETHArtifact.abi).connect(
+    account1
+  ) as WETH;
+
+  await WETH.deployed();
+  await WETH.balanceOf(account1.address).then((n) => console.log(+n));
+  console.log({
+    managerAddress,
+    bankerAddress,
+    wethAddress,
+  });
 };
 
-deployCreator().then(() => {
-  tokenDeployer()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-});
+hardhatDeploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
