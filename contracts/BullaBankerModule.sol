fc6d16e2294214df113826bc6d2f96b4eea13e3a
Colin Nielsen <colinrnielsen@gmail.com>
2021-11-19 17:53:56 -0700

    bullaBankerModule v0.0.1

---
+++ contracts/BullaBankerModule.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+import "@gnosis.pm/zodiac/contracts/core/Module.sol";
+import "./BullaBanker.sol";
+
+/// @title BullaBankerModule
+/// @author Colin Nielsen
+/// @notice A gnosis module for BullaBanker allowing permissionless use of basic BullaBanker functions
+
+contract BullaBankerModule is Module {
+    string public constant VERSION = "0.0.1";
+    address public bullaBanker;
+    address public bullaClaim;
+
+    event BullaBankerModuleSetup(
+        string version,
+        address indexed safe,
+        address indexed moduleAddress,
+        address indexed initiator
+    );
+
+    /// @dev Initialize function, will be triggered when a new proxy is deployed
+    /// @param _safe Address of the safe
+    /// @param _bullaBanker Address of the avatar in this case, a gnosis safe
+    /// @param _bullaClaim Address of the avatar in this case, a gnosis safe
+    /// @notice Designated token address can not be zero
+    constructor(
+        address _safe,
+        address _bullaBanker,
+        address _bullaClaim
+    ) {
+        bytes memory initParams = abi.encode(_safe, _bullaBanker, _bullaClaim);
+        setUp(initParams);
+    }
+
+    function setUp(bytes memory initParams) public override {
+        (address _safe, address _bullaBanker, address _bullaClaim) = abi.decode(
+            initParams,
+            (address, address, address)
+        );
+        require(_safe != address(0), "BULLAMODULE: Zero safe address");
+        __Ownable_init();
+        setAvatar(_safe);
+        setTarget(_safe);
+        transferOwnership(_safe);
+        bullaBanker = _bullaBanker;
+        bullaClaim = _bullaClaim;
+
+        emit BullaBankerModuleSetup(VERSION, _safe, address(this), msg.sender);
+    }
+
+    function createBullaClaim(
+        BullaBanker.ClaimParams calldata _claim,
+        bytes32 _bullaTag,
+        string calldata _tokenUri
+    ) external {
+        //0xa1001a60  =>  createBullaClaim((uint256,address,address,string,uint256,address,(bytes32,uint8,uint8)),bytes32,string)
+        bytes memory data = abi.encodeWithSelector(
+            0xa1001a60,
+            _claim,
+            _bullaTag,
+            _tokenUri
+        );
+        require(
+            exec(bullaBanker, 0, data, Enum.Operation.Call),
+            "BULLAMODULE: Create claim failed"
+        );
+    }
+
+    function updateBullaTag(uint256 _tokenId, bytes32 _bullaTag) external {
+        //0x4fbb8987  =>  updateBullaTag(uint256 tokenId, bytes32 newTag)
+        bytes memory data = abi.encodeWithSelector(
+            0x4fbb8987,
+            _tokenId,
+            _bullaTag
+        );
+        require(
+            exec(bullaBanker, 0, data, Enum.Operation.Call),
+            "BULLAMODULE: Tag update failed"
+        );
+    }
+
+    function rejectClaim(uint256 _tokenId) external {
+        //0x20341101  =>  rejectClaim(uint256 tokenId)
+        bytes memory data = abi.encodeWithSelector(0x20341101, _tokenId);
+        require(
+            exec(bullaClaim, 0, data, Enum.Operation.Call),
+            "BULLAMODULE: Reject failed"
+        );
+    }
+
+    function rescindClaim(uint256 _tokenId) external {
+        //0xe8042ce5  =>  rescindClaim(uint256 tokenId)
+        bytes memory data = abi.encodeWithSelector(0xe8042ce5, _tokenId);
+        require(
+            exec(bullaClaim, 0, data, Enum.Operation.Call),
+            "BULLAMODULE: Rescind Failed"
+        );
+    }
+
+    function approve(address _token, uint256 amount) external {
+        //0x095ea7b3  =>  approve(address,uint256)
+        bytes memory data = abi.encodeWithSelector(
+            0x095ea7b3,
+            address(bullaClaim),
+            amount
+        );
+        require(
+            exec(_token, 0, data, Enum.Operation.Call),
+            "BULLAMODULE: Approve Failed"
+        );
+    }
+}
