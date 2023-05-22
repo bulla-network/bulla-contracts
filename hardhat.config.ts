677a7f4e6075d1baeab360bb1ea114d1f89cc37b
solidoracle <solidoracle17@gmail.com>
2023-05-22 12:01:21 +0200

    bnb deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,12 @@
-require("dotenv").config({ path: "./.env" });
-import { HardhatUserConfig } from "hardhat/types";
-import "@nomiclabs/hardhat-ethers";
-import "@typechain/hardhat";
-import "hardhat-deploy";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-solhint";
-import "hardhat-gas-reporter";
-import "@nomiclabs/hardhat-etherscan";
+require('dotenv').config({ path: './.env' });
+import { HardhatUserConfig } from 'hardhat/types';
+import '@nomiclabs/hardhat-ethers';
+import '@typechain/hardhat';
+import 'hardhat-deploy';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-solhint';
+import 'hardhat-gas-reporter';
+import '@nomiclabs/hardhat-etherscan';
 // import "hardhat-ethernal"
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY!;
@@ -17,120 +17,125 @@ const COINMARKETCAP_API = process.env.COINMARKETCAP_API!;
 const DEPLOYER_ADDRESS = process.env.DEPLOYER_ADDRESS!;
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
-  solidity: {
-    compilers: [{ version: "0.8.7" }, { version: "0.8.3" }],
-    settings: { optimizer: { enabled: true, runs: 200 } },
-  },
-  networks: {
-    /** comment out this hardhat config if running tests */
-    // hardhat: {
-    //   mining: {
-    //     auto: false,
-    //     interval: 1000,
-    //   },
-    // },
-    /** ^^^ */
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [DEPLOY_PK],
-      chainId: 1,
+    defaultNetwork: 'hardhat',
+    solidity: {
+        compilers: [{ version: '0.8.7' }, { version: '0.8.3' }],
+        settings: { optimizer: { enabled: true, runs: 200 } },
     },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [DEPLOY_PK],
-      chainId: 4,
+    networks: {
+        /** comment out this hardhat config if running tests */
+        // hardhat: {
+        //   mining: {
+        //     auto: false,
+        //     interval: 1000,
+        //   },
+        // },
+        /** ^^^ */
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [DEPLOY_PK],
+            chainId: 1,
+        },
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [DEPLOY_PK],
+            chainId: 4,
+        },
+        goerli: {
+            url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [DEPLOY_PK],
+            chainId: 5,
+        },
+        base_goerli: {
+            url: `https://goerli.base.org`,
+            accounts: [DEPLOY_PK],
+            chainId: 84531,
+        },
+        xdai: {
+            url: 'https://rpc.xdaichain.com',
+            accounts: [DEPLOY_PK],
+            chainId: 100,
+        },
+        rsk: {
+            url: `https://rsk.getblock.io/mainnet/?api_key=${GET_BLOCK_API_KEY}`,
+            accounts: [DEPLOY_PK],
+            chainId: 30,
+        },
+        polygon: {
+            url: 'https://polygon-rpc.com/',
+            accounts: [DEPLOY_PK],
+            chainId: 137,
+            gasPrice: 80000000000,
+        },
+        harmony_testnet: {
+            url: 'https://api.s0.b.hmny.io',
+            accounts: [DEPLOY_PK],
+            chainId: 1666700000,
+        },
+        harmony: {
+            url: 'https://a.api.s0.t.hmny.io',
+            accounts: [DEPLOY_PK],
+            chainId: 1666600000,
+        },
+        avalanche_cChain: {
+            url: 'https://api.avax.network/ext/bc/C/rpc',
+            accounts: [DEPLOY_PK],
+            chainId: 43114,
+        },
+        celo: {
+            url: `https://forno.celo.org`,
+            accounts: [DEPLOY_PK],
+            chainId: 42220,
+        },
+        aurora: {
+            url: `https://mainnet.aurora.dev`,
+            accounts: [DEPLOY_PK],
+            chainId: 1313161554,
+        },
+        moonbeam: {
+            url: `https://rpc.api.moonbeam.network`,
+            accounts: [DEPLOY_PK],
+            chainId: 1284,
+        },
+        arbitrum: {
+            url: `https://arb1.arbitrum.io/rpc`,
+            accounts: [DEPLOY_PK],
+            chainId: 42161,
+        },
+        fuse: {
+            url: `https://rpc.fuse.io`,
+            accounts: [DEPLOY_PK],
+            chainId: 122,
+        },
+        bnb: {
+            url: `https://bsc-dataseed.binance.org/`,
+            accounts: [DEPLOY_PK],
+            chainId: 56,
+        },
     },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [DEPLOY_PK],
-      chainId: 5,
+    namedAccounts: {
+        deployer: {
+            default: DEPLOYER_ADDRESS,
+        },
     },
-    base_goerli: {
-      url: `https://goerli.base.org`,
-      accounts: [DEPLOY_PK],
-      chainId: 84531,
+    gasReporter: {
+        enabled: true,
+        currency: 'USD',
+        gasPrice: 100,
+        // coinmarketcap: COINMARKETCAP_API,
     },
-    xdai: {
-      url: "https://rpc.xdaichain.com",
-      accounts: [DEPLOY_PK],
-      chainId: 100,
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+        customChains: [
+            {
+                network: 'base-goerli',
+                chainId: 84531,
+                urls: {
+                    apiURL: 'https://api-goerli.basescan.org/api',
+                    browserURL: 'https://goerli.basescan.org',
+                },
+            },
+        ],
     },
-    rsk: {
-      url: `https://rsk.getblock.io/mainnet/?api_key=${GET_BLOCK_API_KEY}`,
-      accounts: [DEPLOY_PK],
-      chainId: 30,
-    },
-    polygon: {
-      url: "https://polygon-rpc.com/",
-      accounts: [DEPLOY_PK],
-      chainId: 137,
-      gasPrice: 80000000000,
-    },
-    harmony_testnet: {
-      url: "https://api.s0.b.hmny.io",
-      accounts: [DEPLOY_PK],
-      chainId: 1666700000,
-    },
-    harmony: {
-      url: "https://a.api.s0.t.hmny.io",
-      accounts: [DEPLOY_PK],
-      chainId: 1666600000,
-    },
-    avalanche_cChain: {
-      url: "https://api.avax.network/ext/bc/C/rpc",
-      accounts: [DEPLOY_PK],
-      chainId: 43114,
-    },
-    celo: {
-      url: `https://forno.celo.org`,
-      accounts: [DEPLOY_PK],
-      chainId: 42220,
-    },
-    aurora: {
-      url: `https://mainnet.aurora.dev`,
-      accounts: [DEPLOY_PK],
-      chainId: 1313161554,
-    },
-    moonbeam: {
-      url: `https://rpc.api.moonbeam.network`,
-      accounts: [DEPLOY_PK],
-      chainId: 1284,
-    },
-    arbitrum: {
-      url: `https://arb1.arbitrum.io/rpc`,
-      accounts: [DEPLOY_PK],
-      chainId: 42161,
-    },
-    fuse: {
-      url: `https://rpc.fuse.io`,
-      accounts: [DEPLOY_PK],
-      chainId: 122,
-    },
-  },
-  namedAccounts: {
-    deployer: {
-      default: DEPLOYER_ADDRESS,
-    },
-  },
-  gasReporter: {
-    enabled: true,
-    currency: "USD",
-    gasPrice: 100,
-    // coinmarketcap: COINMARKETCAP_API,
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-    customChains: [
-      {
-        network: "base-goerli",
-        chainId: 84531,
-        urls: {
-         apiURL: "https://api-goerli.basescan.org/api",
-         browserURL: "https://goerli.basescan.org"
-        }
-      }
-    ]
-  }
 };
 export default config;
